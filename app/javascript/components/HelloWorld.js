b407bac6d208d2f837f0202f0d5f64c96db2a75d
rioto.nagano <kingdom950927@gmail.com>
2022-11-10 21:13:20 +0900

    テキストの表示

--- app/javascript/components/HelloWorld.js
+++ app/javascript/components/HelloWorld.js
@@ -1,10 +1,39 @@
 import React from "react"
 import PropTypes from "prop-types"
 class HelloWorld extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      greeting: ''
+    };
+  }
+
+  componentDidMount() {
+    fetch(this.props.url)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            greeting: result.greeting
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            error
+          });
+        }
+      )
+  }
+
   render () {
+    const { error, greeting } = this.state;
     return (
       <React.Fragment>
-        Greeting: {this.props.greeting}
+        Greeting: {greeting}
       </React.Fragment>
     );
   }
